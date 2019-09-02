8049076a339bc196cae6c389df7938f5b02cf5ee
Aaron Dunigan AtLee <aaron.dunigan.atlee@gmail.com>
2019-09-02 17:15:07 -0600

    Prototype

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 # Imports
 from adafruit_crickit import crickit
 from random import choice, randint
-from time import
+from time import time, sleep
 
 # Global constants
+# Range of meter
 METER_MIN = 0
 METER_MAX = 10
 # The following is a dictionary that corresponds meter levels to servo angles, 
 # e.g. level 5 is at angle 90.  Adjust this if you want to change the layout of the meter.
 SERVO_ANGLES = {0:0, 1:18, 2:36, 3:54, 4:72, 5:90, 6:108, 7:126, 8:144, 9:162, 10:180}
 # Folder names for music groups:
-MUSIC_GROUP_1_PATH = "/home/pi/love-meter/group1/"
-MUSIC_GROUP_2_PATH = "/home/pi/love-meter/group2/"
-MUSIC_GROUP_3_PATH = "/home/pi/love-meter/group3/"
+GROUP1_PATH = "/home/pi/love-meter/group1/"
+GROUP2_PATH = "/home/pi/love-meter/group2/"
+GROUP3_PATH = "/home/pi/love-meter/group3/"
+GROUP4_PATH = "/home/pi/love-meter/group4/"
 # Assuming servo is connected to servo1 connector.  Change this if using a different port.
 METER_SERVO = crickit.servo1
+# Get current servo angle and call it zero:
+SERVO_ZERO = METER_SERVO.angle
 # Initialize LED connected to neopixel port.
 crickit.init_neopixel(1)
 LED = crickit.neopixel
 # Color definitions
 YELLOW = (255, 255, 0)
 GREEN = (0, 255, 0)
+OFF = (0, 0, 0)
+# Time interval that touchpads need to be touched in order to be triggered, in seconds (decimals allowed):
+TOUCHPAD_SENSITIVITY = 1.0
+HANDLE_SENSITIVITY = TOUCHPAD_SENSITIVITY
+# Input devices
+HANDLE = crickit.touch_1
+TOUCH2 = crickit.touch_2
+TOUCH3 = crickit.touch_3
+TOUCH4 = crickit.touch_4
+# Time in seconds to delay between activating the meter, and resetting it.
+RESET_DELAY = 3
 
 # Global variables
 # Start with a random love level from 5 to 8.
@@ -29,9 +44,10 @@ GREEN = (0, 255, 0)
 love_level = randint(5, 8)
 
 # Functions
-def play_music(file):
-    """ Play the music file via the system sound. """
-    pass
+def play_music(folder):
+    """ Play a random mp3 from the given folder. """
+    # TODO: Implement this.
+    print("Playing music from " + folder)
 
 def meter_increment(love):
     """ Add one to level, but keep within meter range. """
@@ -45,20 +61,58 @@ def set_meter(level, duration):
     
     # Check we've received an appropriate level first. If not, do nothing.
     if level in SERVO_ANGLES:
-        # TODO: turn on yellow LED
-        target_angle = SERVO_ANGLES[level]
+        target_angle = SERVO_ZERO + SERVO_ANGLES[level]
         # TODO: implement time duration
         METER_SERVO.angle = target_angle
-        # TODO: turn off yellow LED
 
 def ready():
     """ Flash green light to indicate ready. """
-    pass
+    for i in range(5):
+        LED.fill(GREEN)
+        sleep(0.5)
+        LED.fill(OFF)
+        sleep(0.5)
+
+def reset():
+    """ Reset meter and LED's. """
+    LED.fill(OFF)
+    set_meter(METER_MIN)
+
+def is_touched(device):
+    """ Check if device is touched for sufficient time. """
+    start_time = time()
+    # Wait until handle is no longer touched.
+    while HANDLE.value == True:
+        pass
+    # Check if elapsed time exceeds sensitivity:
+    if time() - start_time > HANDLE_SENSITIVITY:
+        return True
+    else:
+        return False
+
+def measure_love():
+    LED.fill(YELLOW)
+    set_meter(love_level)
+    LED.fill(GREEN)
+    sleep(RESET_DELAY)
+    reset()
 
 # Main code.
+initialize()
 ready()
-# Set callbacks for devices.  TODO: research this.
-# 
+while True:
+    if is_touched(HANDLE):
+        measure_love()
+    elif is_touched(TOUCH2):
+        play_music(GROUP2_PATH)
+    elif is_touched(TOUCH3):
+        play_music(GROUP3_PATH)
+        love_level = meter_increment(love_level)
+    elif is_touched(TOUCH4):
+        play_music(GROUP4_PATH)
+        love_level = METER_MAX
+    
+    
 
     
 
