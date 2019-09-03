a251b6026f956385a2f9a2f229a1936a4ad6bfcb
Aaron Dunigan AtLee <aaron.dunigan.atlee@gmail.com>
2019-09-03 09:07:30 -0600

    text assist

--- test_audio.py
+++ test_audio.py
@@ -1,6 +1,7 @@
 import pygame
 pygame.mixer.init()
 pygame.mixer.music.load("temple-bell.mp3")
+print("You should hear a bell sound.")
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy() == True:
     continue
